c066f23924c8838031271d9fb0a81fc5af057b91
xmzs <1195566313@qq.com>
2023-07-17 21:30:20 +0800

    init

---
+++ .vitepress/config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vitepress'
+import {webMenu} from './menu/web'
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "xm-blog",
+  description: "AllCode",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' }
+    ],
+
+    sidebar:{
+        '/web/':[
+            ...webMenu,
+        ],
+        '/server/':[
+            ...webMenu,
+        ]
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ .vitepress/menu/web.ts
@@ -0,0 +1,17 @@
+export const webMenu = [
+    {
+        text: "vue3",
+        link:'/web/vue3/index',
+        collapsed: false,
+        items: [
+            {
+                text: "基础导读",
+                link: "/web/vue3/base"
+            },
+            {
+                text: "三十九章(electron)",
+                link: "/web/vue3/electron"
+            },
+        ]
+    }
+]
