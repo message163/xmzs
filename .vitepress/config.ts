235aea78e34ed991741806af808d189ca3b9ee17
xmzs <1195566313@qq.com>
2023-07-17 21:54:19 +0800

    init

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,7 +4,7 @@ import {webMenu} from './menu/web'
 export default defineConfig({
   title: "xm-blog",
   description: "AllCode",
-  base:'./',
+  base:'/dist/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
